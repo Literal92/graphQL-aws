967e3cc9fe904710dd3fd832b7895f480c37f19c
amir tavakoli <am.tavakoli1992@outlook.com>
2023-02-07 17:12:10 +0400

    add graphql config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import awsmobile from 'src/aws-exports';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amplify-aws';
+
+  constructor() {
+    Amplify.configure(awsmobile);
+  }
 }
